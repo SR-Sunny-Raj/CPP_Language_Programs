b6d0deb6cf18c874912fb8c828eb60d9d87dc737
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-02-04 23:18:21 +0530

    Function call by reference in c++

---
+++ Function/Function call by reference.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//Function "Call by reference" in C++
+int sum(int &,int &);
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers : ";
+    cin>>a>>b;
+    cout<<"The sum of the numbers is : "<<sum(a,b);
+    return 0;
+}
+int sum(int &p,int &q)
+{
+    return(p+q);
+}
