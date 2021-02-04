81858e3a26c4cd777a0d5ec0b69b40f2b00661cf
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-02-04 22:48:42 +0530

    Addition of two numbers

---
+++ Simple Basic Programs/Addition of two numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//Simple addition of two numbers
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers : ";
+    cin>>a>>b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<a+b;
+    return 0;
+}
