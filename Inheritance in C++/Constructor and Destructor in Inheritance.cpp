6d99e4747e4c50e1a53aacc198dd078e85b798a2
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-02-10 16:36:03 +0530

    Constructor and Destructor in Inheritance in C++

---
+++ Inheritance in C++/Constructor and Destructor in Inheritance.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+//Constructor and Destructor in inheritance in C++
+class A
+{
+    private:
+    int a;
+    public:
+    A(int k)
+    {
+        a=k;
+    }
+    void show_data_a()
+    {
+        cout<<"a = "<<a<<endl;
+    }
+};
+class B : public A
+{
+    private:
+    int b;
+    public:
+    B(int x,int y):A(x)
+    {
+        b=y;
+    }
+    void show_data()
+    {
+        show_data_a();
+        cout<<"b = "<<b;
+    }
+};
+int main()
+{
+    int m,n;
+    cout<<"Enter any two number : ";
+    cin>>m>>n;
+    B obj(m,n);
+    obj.show_data();
+    return 0;
+}
