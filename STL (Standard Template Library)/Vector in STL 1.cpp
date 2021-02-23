137543178e8933568fb34c608831e64ebe9c1a39
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-02-24 02:04:21 +0530

    Create Vector in STL 1.cpp

---
+++ STL (Standard Template Library)/Vector in STL 1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//Vector in STL in C++
+//push_back() , pop_back() , capacity() , aize()
+int main()
+{
+    int i;
+    vector <int> v1;
+    cout<<"Capacity is -:"<<v1.capacity()<<endl;
+    for(i=0;i<=9;i++)
+    {
+        v1.push_back(10*(i+1));
+    }
+    cout<<"Current Capacity is -: "<<v1.capacity()<<endl;
+    for(i=0;i<=9;i++)
+    {
+        cout<<v1[i]<<endl;
+    }
+    v1.pop_back();
+    cout<<"Current Capacity is -: "<<v1.capacity()<<endl;
+    v1.pop_back();
+    cout<<"Current Capacity is -: "<<v1.capacity()<<endl;
+    v1.pop_back();
+    cout<<"Current Capacity is -: "<<v1.capacity()<<endl;
+    cout<<"Total elements remaining is -: "<<v1.size()<<endl;
+    for(i=0;i<=v1.size();i++)
+    {
+        cout<<v1[i]<<endl;
+    }
+    return 0;
+}
