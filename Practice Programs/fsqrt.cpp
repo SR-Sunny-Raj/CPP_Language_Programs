412739b0577b2750d6c6cb09a0514ad9de159239
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-27 01:23:36 +0530

    finding squareroot

---
+++ Practice Programs/fsqrt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int x;
+        cin >> x;
+        cout << int(sqrt(x)) << endl;
+    }
+    return 0;
+}
