4b9165879b6d28202d3f90395c51eaa499709512
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-27 01:23:53 +0530

    packaging cupcakes

---
+++ Practice Programs/packaging_cupcakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << (N / 2) + 1 << endl;
+    }
+    return 0;
+}
