58ae89c34d07c942e32cc7a6e3f604a26ad1f778
Sunny Raj <sraj1333.sr@gmail.com>
2022-06-27 13:08:21 +0530

    Practice Programs

---
+++ Practice Programs/set_matrix_zero.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void setZeros(vector<vector<int>> &matrix)
+{
+    set<int> arr, brr;
+    int m = matrix.size(), n = matrix[0].size();
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                arr.insert(i);
+                brr.insert(j);
+            }
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr.count(i) != 0 || brr.count(j) != 0)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
